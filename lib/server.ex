97e25e02b9873b61c5abdf49e907ed909138f41c
Mario Anticoli <mario@mario.local>
2020-02-09 18:18:57 +0100

    sync calls

--- lib/server.ex
+++ lib/server.ex
@@ -5,14 +5,18 @@ defmodule PubSub.Server do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
-  def handle_cast({:join_topic, topic, pid}, state) do
+  def handle_call({:join_topic, topic, pid}, _from, state) do
     new_list =
       case subscribers(state, topic) do
         [] -> [pid]
         l -> [pid | l] |> Enum.dedup()
       end
 
-    {:noreply, state |> Map.put(topic, new_list)}
+    {:reply, new_list, state |> Map.put(topic, new_list)}
+  end
+
+  def handle_call({:list_subscribers, topic}, _from, state) do
+    {:reply, subscribers(state, topic), state}
   end
 
   def handle_cast({:broadcast, topic, msg}, state) do
@@ -24,17 +28,13 @@ defmodule PubSub.Server do
     {:noreply, state}
   end
 
-  def handle_call({:list_subscribers, topic}, _from, state) do
-    {:reply, subscribers(state, topic), state}
-  end
-
   def init(state), do: {:ok, state}
 
-  def join_topic(topic, pid), do: GenServer.cast(__MODULE__, {:join_topic, topic, pid})
-
-  def broadcast(topic, msg), do: GenServer.cast(__MODULE__, {:broadcast, topic, msg})
+  def join_topic(topic, pid), do: GenServer.call(__MODULE__, {:join_topic, topic, pid})
 
   def list_subscribers(topic), do: GenServer.call(__MODULE__, {:list_subscribers, topic})
+
+  def broadcast(topic, msg), do: GenServer.cast(__MODULE__, {:broadcast, topic, msg})
 
   defp subscribers(s, t) do
     case s |> Map.get(t) do
