0a80beffa347b7981b6b842b98febb0daacac687
Mario Anticoli <mario@mario.local>
2020-02-12 03:30:10 +0100

    test for wildcards

--- test/pubsub_test.exs
+++ test/pubsub_test.exs
@@ -92,24 +92,233 @@ defmodule PubSubTest do
   end
 
   test "Subscribers to wildcard topics should receive all matching messages" do
-    assert 1 == 2
+    subscription = "/home/bedroom/+"
+    client1 = Subscriber.create([subscription])
+    Subscriber.list(subscription) |> IO.inspect()
+    Publisher.publish("/home/bedroom/temperature", "30")
+    Publisher.publish("/home/bedroom/humidity", "40")
+
+    res1 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    res2 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res1 == {client1, "30"}
+    assert res2 == {client1, "40"}
   end
 
   describe "Subscribers with wildcard in the middle of the topic should receive matching messages." do
     test "Single wildcard" do
-      assert 1 == 2
+      subscription = "/home/+/temperature"
+      client1 = Subscriber.create([subscription])
+      Subscriber.list(subscription) |> IO.inspect()
+
+      Publisher.publish("/home/bedroom/temperature", "30")
+      Publisher.publish("/home/garage/temperature", "40")
+      Publisher.publish("/home/bedroom/humidity", "100")
+
+      res1 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      res2 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      res3 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      assert res1 == {client1, "30"}
+      assert res2 == {client1, "40"}
+      assert res3 == "No message received"
     end
 
     test "Multiple widlcards" do
-      assert 1 == 2
+      subscription = "/home/+/temperature/+"
+      client1 = Subscriber.create([subscription])
+      Subscriber.list(subscription) |> IO.inspect()
+
+      Publisher.publish("/home/bedroom/temperature/celsius", "30")
+      Publisher.publish("/home/garage/temperature/fahrenheit", "40")
+      Publisher.publish("/home/garage/temperature", "-1")
+      Publisher.publish("/home/garage/humidity/rh", "-1")
+      Publisher.publish("/office/garage/temperature/celsius", "-1")
+
+      res1 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      assert res1 == {client1, "30"}
+
+      res2 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      assert res2 == {client1, "40"}
+
+      res3 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      assert res3 == "No message received"
+
+      res4 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      assert res4 == "No message received"
+
+      res5 =
+        receive do
+          r -> r
+        after
+          1_000 -> "No message received"
+        end
+
+      assert res5 == "No message received"
     end
   end
 
   test "Subscribing to wildcard # receives all messages" do
-    assert 1 == 2
+    client1 = Subscriber.create(["/home/#"])
+    Subscriber.list("/home/#") |> IO.inspect()
+
+    Publisher.publish("/home/bedroom/temperature", "30")
+    Publisher.publish("/home/bedroom/humidity", "40")
+    Publisher.publish("/office/table/humidity", "50")
+
+    res1 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res1 == {client1, "30"}
+
+    res2 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res2 == {client1, "40"}
+
+    res3 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res3 == "No message received"
   end
 
   test "Subscribing to wild combination of wildcards." do
-    assert 1 == 2
+    client1 = Subscriber.create(["/group/+/home/+/#"])
+    Subscriber.list("/group/+/home/+/#") |> IO.inspect()
+
+    Publisher.publish("/group/1/home/bedroom/humidity", "80")
+    Publisher.publish("/group/1/home/bedroom/temperature", "30")
+    Publisher.publish("/group/1/home/bedroom/temperature/celsius", "30")
+    Publisher.publish("/group/1/home/bedroom/temperature/fahrenheit", "30")
+    Publisher.publish("/home/bedroom/humidity", "40")
+    Publisher.publish("/office/table/humidity", "50")
+    Publisher.publish("/group/1/office/bedroom/humidity", "-1")
+
+    res1 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res1 == {client1, "80"}
+
+    res2 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res2 == {client1, "30"}
+
+    res3 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res3 == {client1, "30"}
+
+    res4 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res4 == {client1, "30"}
+
+    res5 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res5 == "No message received"
+
+    res6 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res6 == "No message received"
+
+    res7 =
+      receive do
+        r -> r
+      after
+        1_000 -> "No message received"
+      end
+
+    assert res7 == "No message received"
   end
 end
